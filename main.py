3211cbceb311bf83e00ab60f085493791d7a3a3f
SFWR <45100938+SFWR-online@users.noreply.github.com>
2022-08-06 23:47:26 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import pyautogui
+import cv2
+import numpy as np
+import sys
+while 1:
+	img = pyautogui.screenshot(region=[600,200, 800, 800])  # 分别代表：左上角坐标，宽高
+	img.save("test_screen.jpg")
+	img = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+	himg = cv2.cvtColor(np.asarray(img), cv2.COLOR_BGR2HSV)
+	thresh1 = np.array([140, 120, 120])                       # 目标红旗的低阈值
+	thresh2 = np.array([160, 255, 255])                      # 目标红旗的高阈值
+	img_flag = cv2.inRange(himg, thresh1, thresh2)  
+	img_morph = img_flag.copy()                             # 复制图像
+	cv2.erode(img_morph, (3,3), img_morph, iterations= 3)   # 腐蚀运算
+	cv2.dilate(img_morph, (3,3), img_morph, iterations= 3)  # 膨胀运算
+	cnts, _ = cv2.findContours(img_morph, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # 获取图像轮廓
+	cnts_sort = sorted(cnts, key= cv2.contourArea, reverse= True) # 将轮廓包含面积从大到小排列
+	if len(cnts_sort):
+		box = cv2.minAreaRect(cnts_sort[0])
+		points = np.int0(cv2.boxPoints(box))
+		cen_v = (points[0,0] + points[2,0]) / 2
+		cen_h = (points[0,1] + points[2,1]) / 2
+		cv2.drawContours(img, [points], -1, (255,0,0), 2)
+		pyautogui.moveTo(600+cen_v, 200+cen_h, duration=0)
+	    #cv2.drawContours(img, [points], -1, (255,0,0), 2)
+	cv2.waitKey(0)
