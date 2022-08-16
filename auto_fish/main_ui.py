92a1035315732549b7254b1cb463f1780e53b96e
SFWR-online <1207708796@qq.com>
2022-08-16 11:40:16 +0800

    revise af_main,add af_main_ui

--- auto_fish/main_ui.py
+++ auto_fish/main_ui.py
@@ -5,12 +5,31 @@ import time
 import keyboard
 import win32api
 import win32con 
+import tkinter
 p=0
 q=0
 flag=1
-while 1:
+t1=time.time()
+t2=time.time()
+root_window = tkinter.Tk()
+root_window.geometry("100x60+1700+80")#设置窗口大小x*y和位置x+y
+root_window.attributes('-topmost', 'true')#控件始终浮于上层
+root_window.resizable(False, False)#删除最小化与最大化
+var = tkinter.StringVar()
+var.set("close")
+my_show = tkinter.Label(root_window,textvariable=var,font=('Arial',12),width=10,height=2)
+my_show.pack()
+my_show.config(fg="green")
+def change_text():
+	global flag,q,p,t1,t2		
 	if keyboard.is_pressed('n'):
 		flag=1^flag
+		if flag==0:
+			t1=time.time()
+			var.set("open")
+		if flag==1:
+			var.set("close")
+			my_show.config(fg="green")
 		time.sleep(2)
 	if flag==1:
 		time.sleep(0.5)
@@ -21,7 +40,6 @@ while 1:
 		thresh1 = np.array([0, 120, 120])     
 		thresh2 = np.array([10, 255, 255]) 
 		img_flag = cv2.inRange(himg, thresh1, thresh2)  
-		#滤波
 		img_morph = img_flag.copy()                             # 复制图像
 		cv2.erode(img_morph, (3,3), img_morph, iterations= 3)   # 腐蚀运算
 		cv2.dilate(img_morph, (3,3), img_morph, iterations= 3)  # 膨胀运算
@@ -49,4 +67,10 @@ while 1:
 			pyautogui.moveTo(x+1200, y, duration=0.1)
 			pyautogui.moveTo(x-1200, y, duration=0.1)
 			p=0
+			t2=time.time()
+			if t2-t1>=180:
+				my_show.config(fg="red")
 		cv2.waitKey(0)
+	my_show.after(50, func=change_text)
+my_show.after(50, func=change_text)
+root_window.mainloop()
